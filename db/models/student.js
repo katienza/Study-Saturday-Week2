5e2526aa04faec2997f76d744c3e358e3898bf4f
Ken Atienza <atienza.ken@gmail.com>
2018-11-17 12:00:37 -0500

    c1

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,33 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+// createdb 'study-saturdays' first step ALWAYS
+
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+});
+
+Student.beforeCreate((name) => {
+  const firstName = name.firstName;
+  const lastName = name.lastName;
+  name.firstName = firstName.slice(0, 1).toUpperCase() + firstName.slice(1);
+  name.lastName = lastName.slice(0, 1).toUpperCase() + lastName.slice(1);
+});
 
 module.exports = Student;
--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,27 @@
 const router = require('express').Router();
+const Student = require('../db/models/student');
 
 
+router.get('/', async (req, res, next) => {
+  try {
+    const students = await Student.findAll();
+    res.status(200).json(students);
+  } catch(err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    if (!Student) {
+      const student = req.params.id;
+      res.json(student);
+    } else {
+      res.status(404).json('Not Found');
+    }
+  } catch(err) {
+    next(err);
+  }
+
+});
 module.exports = router;
